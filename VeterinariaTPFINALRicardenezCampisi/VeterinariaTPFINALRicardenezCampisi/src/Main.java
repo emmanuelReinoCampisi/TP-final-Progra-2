a7c7ec465a7eae39ad21bc3705c1b4de1013f5d9
Emmanuel <reinocampisiemmanuel@gmail.com>
2025-11-05 17:20:25 -0300

    importe al main los package
    Importe al main los package para que no tire error al querer instanciar o querer usar algun metodo

--- VeterinariaTPFINALRicardenezCampisi/VeterinariaTPFINALRicardenezCampisi/src/Main.java
+++ VeterinariaTPFINALRicardenezCampisi/VeterinariaTPFINALRicardenezCampisi/src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 import Clases.Duenio;
+import Clases.Cita;
+import Clases.Empleado;
+import Clases.Mascota;
+import Clases.Persona;
+import Clases.Veterinario;
+import Enumeradores.ESPECIE;
+import Enumeradores.ESTADOCITA;
+import Enumeradores.TURNO;
+import Excepciones.ExcepcionNoExistente;
+import Excepciones.ExcepcionYaExistente;
+import Excepciones.ExcepcionColeccionVacia;
 
 public class Main {
     public static void main(String[] args) {
