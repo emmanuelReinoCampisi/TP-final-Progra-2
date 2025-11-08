8a711b311ea69bbee444d38a62d0b6548943fcbc
Emmanuel <reinocampisiemmanuel@gmail.com>
2025-11-08 15:07:06 -0300

    Testeo del metodo asignarCita

--- VeterinariaTPFINALRicardenezCampisi/VeterinariaTPFINALRicardenezCampisi/src/Main.java
+++ VeterinariaTPFINALRicardenezCampisi/VeterinariaTPFINALRicardenezCampisi/src/Main.java
@@ -6,6 +6,7 @@ import Enumeradores.ESPECIE;
 import Enumeradores.ESTADOCITA;
 import Enumeradores.TIPOCITA;
 import Enumeradores.TURNO;
+import Excepciones.CitaInvalidaExcep;
 import org.json.JSONObject;
 
 import java.time.LocalDate;
@@ -22,17 +23,42 @@ public class Main {
         }catch (Exception e){
             e.printStackTrace();
         }
-        LocalDate fechaCita = LocalDate.of(2025,11,7);
-        LocalTime horarioCita = java.time.LocalTime.of(10,30);
+        Veterinaria veterinaria = new Veterinaria();
+
+        LocalDate fechaCita = LocalDate.of(2025,11,8);
+        LocalDate fechaCita2 = LocalDate.of(2025,11,8);
+        LocalTime horarioCita = java.time.LocalTime.of(15,30);
+        LocalTime horarioCita2 = java.time.LocalTime.of(15,30);
         String motivo = "Esta cita fue creada para testeo";
 
+        Veterinario Vettester =new Veterinario("Juan",28,1234,"juan123@gmail.com","1234", TURNO.TARDE,"Veterinario");
+        Veterinario Vettester2 =new Veterinario("Pedro",31,412,"pepe533@gmail.com","654", TURNO.TARDE,"Veterinario");
         Mascota TobyTester = new Mascota("TobyTester",2, ESPECIE.CANINO,"Rottweiler",11223344);
-        Cita cita = new Cita (fechaCita,horarioCita, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,new Veterinario("Juan",28,1234,"juan123@gmail.com","1234", TURNO.MANIANA,"Veterinario"));
+        Mascota CocoTester = new Mascota("CocoTester",1, ESPECIE.CANINO,"Pastor Aleman",11223344);
+        Cita cita = new Cita (fechaCita,horarioCita, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,Vettester);
 
         System.out.println(cita.toString());
 
         JSONObject jsonObject = cita.citaTOJson();
         System.out.println("json object cita:");
         System.out.println(jsonObject);
+
+
+        try{    // asi tira eror por el veterinario ocupado
+            /*veterinaria.agregarCita(fechaCita,horarioCita, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,Vettester);
+                veterinaria.agregarCita(fechaCita2,horarioCita2, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,Vettester);
+                */
+
+                /* asi por la mascota que ya tiene asignada una cita
+                veterinaria.agregarCita(fechaCita,horarioCita, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,Vettester);
+                veterinaria.agregarCita(fechaCita2,horarioCita2, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,Vettester);
+                */
+            // aca anda bien 
+            veterinaria.agregarCita(fechaCita,horarioCita, TIPOCITA.CONTROL, TobyTester, ESTADOCITA.PENDIENTE,Vettester);
+            veterinaria.agregarCita(fechaCita,horarioCita, TIPOCITA.CONTROL, CocoTester, ESTADOCITA.PENDIENTE,Vettester2);
+
+        }catch (CitaInvalidaExcep c){
+            System.out.println(""+c.getMessage());
+        }
     }
 }
