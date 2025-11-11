3bd0aab547a7ce243e2f4cad1ad44d8f7d49ffab
Emmanuel <reinocampisiemmanuel@gmail.com>
2025-11-11 18:23:24 -0300

    Update Main.java

--- VeterinariaTPFINALRicardenezCampisi/VeterinariaTPFINALRicardenezCampisi/src/Main.java
+++ VeterinariaTPFINALRicardenezCampisi/VeterinariaTPFINALRicardenezCampisi/src/Main.java
@@ -16,6 +16,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
         Scanner sc = new Scanner(System.in);
         Veterinaria veterinaria = new Veterinaria();
         boolean salir = false;
